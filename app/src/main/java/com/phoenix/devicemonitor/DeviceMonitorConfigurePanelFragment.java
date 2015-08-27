e6ea0e01a9445e60db5a8d82a9244f06cc495db4
ZhengPhoenix <phoenix_zh@foxmail.com>
2015-08-27 16:07:05 +0800

    add switch control and DevicePolicyManager activate method

--- app/src/main/java/com/phoenix/devicemonitor/DeviceMonitorConfigurePanelFragment.java
+++ app/src/main/java/com/phoenix/devicemonitor/DeviceMonitorConfigurePanelFragment.java
@@ -1,15 +1,21 @@
 package com.phoenix.devicemonitor;
 
 import android.app.Fragment;
+import android.app.admin.DevicePolicyManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class DeviceMonitorConfigurePanelFragment extends Fragment {
+public class DeviceMonitorConfigurePanelFragment extends Fragment{
+
+    private Switch mAdminSwitch;
 
     public DeviceMonitorConfigurePanelFragment() {
     }
@@ -17,6 +23,27 @@ public class DeviceMonitorConfigurePanelFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_device_monitor_configure_panel, container, false);
+        View view = inflater.inflate(R.layout.fragment_device_monitor_configure_panel, container, false);
+
+        mAdminSwitch = (Switch) view.findViewById(R.id.pattern_switch);
+
+        mAdminSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    activeAdminManager();
+                }
+            }
+        });
+
+        return view;
+    }
+
+    private void activeAdminManager(){
+        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+
+        startActivityForResult(intent, 0);
     }
 }
+
+
---
+++ app/src/main/java/com/phoenix/devicemonitor/receiver/PatternLockMonitorReceiver.java
@@ -0,0 +1,35 @@
+package com.phoenix.devicemonitor.receiver;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by hui1.zheng on 8/27/2015.
+ */
+public class PatternLockMonitorReceiver extends DeviceAdminReceiver{
+
+    private final String TAG = "MonitorReceiver";
+
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        super.onEnabled(context, intent);
+
+        Log.d(TAG, "Enabled");
+    }
+
+    @Override
+    public void onPasswordFailed(Context context, Intent intent) {
+        super.onPasswordFailed(context, intent);
+
+        Log.d(TAG, "PasswordFailed");
+    }
+
+    @Override
+    public void onPasswordSucceeded(Context context, Intent intent) {
+        super.onPasswordSucceeded(context, intent);
+
+        Log.d(TAG, "PasswordSucceded");
+    }
+}
