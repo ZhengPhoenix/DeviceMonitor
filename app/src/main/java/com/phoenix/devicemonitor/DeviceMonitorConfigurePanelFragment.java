2a324bbb8b35d0461d77762a78bbd51995b80f30
ZhengPhoenix <phoenix_zh@foxmail.com>
2015-08-27 10:54:52 +0800

    Initial commit

---
+++ app/src/main/java/com/phoenix/devicemonitor/DeviceMonitorConfigurePanelFragment.java
@@ -0,0 +1,22 @@
+package com.phoenix.devicemonitor;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DeviceMonitorConfigurePanelFragment extends Fragment {
+
+    public DeviceMonitorConfigurePanelFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_device_monitor_configure_panel, container, false);
+    }
+}
